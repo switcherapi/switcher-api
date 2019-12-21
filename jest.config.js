e9d9bc696492f696f0c16571e840ffc45f0e33ba
petruki <31597636+petruki@users.noreply.github.com>
2019-12-20 17:59:29 -0800

    Fixed erros pointer by Sonar.

--- jest.config.js
+++ jest.config.js
@@ -18,10 +18,14 @@ module.exports = {
   clearMocks: true,
 
   // Indicates whether the coverage information should be collected while executing the test
-  // collectCoverage: true,
+  collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  // collectCoverageFrom: null,
+  collectCoverageFrom: [
+    "**/*.{js,jsx}",
+    "!**/node_modules/**",
+    "!**/vendor/**"
+  ],
 
   // The directory where Jest should output its coverage files
   // coverageDirectory: "coverage",
