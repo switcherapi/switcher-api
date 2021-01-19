f084ec4f5a071c5955898c86a098badc0ba809db
petruki <31597636+petruki@users.noreply.github.com>
2021-01-19 13:48:48 -0800

    Refactored Component router to use controller layer

--- src/controller/component.js
+++ src/controller/component.js
@@ -1,4 +1,7 @@
+import { checkComponent } from '../external/switcher-api-facade';
 import Component from '../models/component';
+import { ActionTypes, RouterTypes } from '../models/role';
+import { formatInput, verifyOwnership } from '../routers/common';
 import { response } from './common';
 
 export async function getComponentById(id) {
@@ -8,4 +11,50 @@ export async function getComponentById(id) {
 
 export async function getComponents(where) {
     return await Component.find(where);
+}
+
+export async function createComponent(args, admin) {
+    let component = new Component({
+        ...args,
+        owner: admin._id
+    });
+
+    await checkComponent(args.domain);
+    component.name = formatInput(component.name);
+    component = await verifyOwnership(admin, component, args.domain, ActionTypes.CREATE, RouterTypes.COMPONENT);
+    const apiKey = await component.generateApiKey();
+    await component.save();
+
+    return { component, apiKey };
+}
+
+export async function updateComponent(id, args, admin) {
+    let component = await getComponentById(id);
+    component = await verifyOwnership(
+        admin, component, component.domain, ActionTypes.UPDATE, RouterTypes.COMPONENT);
+
+    const updates = Object.keys(args);
+    updates.forEach((update) => component[update] = args[update]);
+    component.name = formatInput(component.name);
+    await component.save();
+    return component;
+}
+
+export async function deleteComponent(id, admin) {
+    let component = await getComponentById(id);
+    component = await verifyOwnership(
+        admin, component, component.domain, ActionTypes.DELETE, RouterTypes.COMPONENT);
+
+    await component.remove();
+    return component;
+}
+
+export async function generateApiKey(id, admin) {
+    let component = await getComponentById(id);
+    component = await verifyOwnership(
+        admin, component, component.domain, ActionTypes.UPDATE, RouterTypes.COMPONENT);
+    component.updatedBy = admin.email;
+
+    const apiKey = await component.generateApiKey();
+    return apiKey;
 }
