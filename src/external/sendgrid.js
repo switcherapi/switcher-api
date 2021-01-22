a770f11bc0517c0a0bced767c1c7ee5ca5c04e68
petruki <31597636+petruki@users.noreply.github.com>
2021-01-22 14:22:49 -0800

    Fixes #124

--- src/external/sendgrid.js
+++ src/external/sendgrid.js
@@ -1,8 +1,6 @@
-import sgMail from '@sendgrid/mail';
 import axios from 'axios';
 
 export const sendGridApiUrl = 'https://api.sendgrid.com/v3/mail/send';
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 export function sendAuthCode(email, name, code) {
     sendMail(email, name, code, process.env.SENDGRID_CONFIRMATION_TEMPLATE);
@@ -13,21 +11,20 @@ export function sendAccountRecoveryCode(email, name, code) {
 }
 
 function sendMail(email, name, code, template_id) {
-    axios.post(sendGridApiUrl, null, { 
+    axios.post(sendGridApiUrl, {
+        from: { email: process.env.SENDGRID_MAIL_FROM },
+        personalizations: [{
+            to: [{ email }],
+            dynamic_template_data: {
+                code,
+                name
+            }
+        }],
+        template_id
+    }, { 
         headers: {
             Authorization: `Bearer ${process.env.SENDGRID_API_KEY}`,
             'Content-Type': 'application/json'
-        },
-        data: {
-            from: { email: process.env.SENDGRID_MAIL_FROM },
-            personalizations: [{
-                to: [{ email }],
-                dynamic_template_data: {
-                    code,
-                    name
-                }
-            }],
-            template_id
         }
     });
 }
