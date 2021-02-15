187ed52eb3029a242e478324fa3705effc0e6317
petruki <31597636+petruki@users.noreply.github.com>
2021-02-15 11:14:50 -0800

    Refactored admin to use controller layer

--- src/external/google-recaptcha.js
+++ src/external/google-recaptcha.js
@@ -2,13 +2,13 @@ import axios from 'axios';
 
 export const url = 'https://www.google.com/recaptcha/api/siteverify';
 
-export async function validate_token(req) {
-    if (req.body.token === null || req.body.token === undefined) {
+export async function validate_token(token, remoteAddress) {
+    if (token === null || token === undefined) {
         throw new GoogleRecaptchaError('Token is empty or invalid');
     }
 
     const response = await axios.post(
-        `${url}?secret=${process.env.GOOGLE_RECAPTCHA_SECRET}&response=${req.body.token}&remoteip=${req.connection.remoteAddress}`, null,
+        `${url}?secret=${process.env.GOOGLE_RECAPTCHA_SECRET}&response=${token}&remoteip=${remoteAddress}`, null,
         { 
             headers: {
                 accept: 'application/json'
