3ce690519f55b8c1b8257fcf3201a3165a725f18
petruki <31597636+petruki@users.noreply.github.com>
2021-06-03 22:03:37 -0700

    Fixed code smell

--- src/controller/domain.js
+++ src/controller/domain.js
@@ -64,7 +64,7 @@ export async function transferDomain(args, admin) {
 
     domain.updatedBy = admin.email;
     domain.transfer = domain.transfer ? null : true;
-    return await domain.save();
+    return domain.save();
 }
 
 export async function transferDomainAccept(args, admin) {
