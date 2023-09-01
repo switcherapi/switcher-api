45966f96f28d6b33065b6386170ee6ef4915c530
Roger Floriano <31597636+petruki@users.noreply.github.com>
2023-08-31 22:33:46 -0700

    Bump deps - fixed/upgraded rate_limte to v7 (#441)
    * Bump deps - fixed/upgraded rate_limte to v7
    
    * Fixes code smell
    
    * Added rate limit test

--- src/middleware/limiter.js
+++ src/middleware/limiter.js
@@ -5,11 +5,18 @@ const ERROR_MESSAGE = {
 	error: 'API request per minute quota exceeded'
 };
 
+const getMaxRate = (rate_limit) => {
+	if (rate_limit === 0)
+		return parseInt(DEFAULT_RATE_LIMIT);
+
+	return rate_limit;
+};
+
 export const DEFAULT_RATE_LIMIT = 1000;
 
 export const defaultLimiter = rateLimit({
 	windowMs: DEFAULT_WINDOWMS,
-	max: parseInt(process.env.MAX_REQUEST_PER_MINUTE || DEFAULT_RATE_LIMIT),
+	max: getMaxRate(parseInt(process.env.MAX_REQUEST_PER_MINUTE)),
 	standardHeaders: true,
     message: ERROR_MESSAGE,
 	store: new MemoryStore(),
@@ -18,7 +25,8 @@ export const defaultLimiter = rateLimit({
 export const clientLimiter = rateLimit({
 	windowMs: DEFAULT_WINDOWMS,
 	keyGenerator: (request) => request.domain,
-	max: (request) => request.rate_limit,
+	max: (request) => getMaxRate(request.rate_limit),
+	skip: (request) => request.rate_limit === 0,
 	standardHeaders: true,
     message: ERROR_MESSAGE,
 	store: new MemoryStore(),
