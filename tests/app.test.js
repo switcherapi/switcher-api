dec401922e6b0ef4bc2cd808b24b01f1d462bca8
Roger Floriano <31597636+petruki@users.noreply.github.com>
2023-11-19 13:12:03 -0800

    Bump deps - axios@1.6.2, express-rate-limit@7.1.4, others (#466)

---
+++ tests/app.test.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+import app from '../src/app';
+import request from 'supertest';
+
+afterAll(async () => { 
+  await new Promise(resolve => setTimeout(resolve, 1000));
+  await mongoose.disconnect();
+});
+
+describe('Testing app [REST] ', () => {
+  test('APP_SUITE - Should return success on a health check request', async () => {
+    const req = await request(app)
+      .get('/check')
+      .expect(200);
+
+    expect(req.statusCode).toBe(200);
+    expect(req.body.status).toEqual('UP');
+  });
+
+  test('APP_SUITE - Should return success on a health check request with details', async () => {
+    const req = await request(app)
+      .get('/check?details=1')
+      .expect(200);
+
+    expect(req.statusCode).toBe(200);
+    expect(req.body.status).toEqual('UP');
+    expect(req.body.attributes).toBeDefined();
+  });
+
+  test('APP_SUITE - Should return 404 - Operation not found', async () => {
+    const req = await request(app)
+      .get('/not-found')
+      .expect(404);
+
+    expect(req.statusCode).toBe(404);
+    expect(req.body.error).toEqual('Operation not found');
+  });
+});
