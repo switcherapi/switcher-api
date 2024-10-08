709b016802739a75a71d3a2086593f73de050b15
petruki <31597636+petruki@users.noreply.github.com>
2024-10-07 19:18:03 -0700

    GitOps Account - added https agent for secured request to GitOps API

--- src/external/gitops.js
+++ src/external/gitops.js
@@ -1,8 +1,14 @@
 import axios from 'axios';
+import https from 'https';
+
+const agent = (url) => {
+    const usesHttps = url.startsWith('https://');
+    return new https.Agent({ rejectUnauthorized: !usesHttps });
+};
 
 export async function createAccount(account) {
     const url = `${process.env.SWITCHER_GITOPS_URL}/account`;
-    const response = await axios.post(url, account);
+    const response = await axios.post(url, account, { httpsAgent: agent(url) });
 
     if (response.status !== 201) {
         throw new GitOpsError(`Failed to create account [${response.status}] ${JSON.stringify(response.data)}`);
